66645e9ab52935fe672be9ca523f8611d0309d97
NguyenChuong2411 <nguyenchuong10072411@gmail.com>
2025-09-29 15:20:52 +0700

    Online test Service

---
+++ MainSolution/ModelClass/Connection/OnlineTestDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ModelClass.test;
+
+namespace ModelClass.connection
+{
+    public class OnlineTestDbContext : DbContext
+    {
+        public OnlineTestDbContext(DbContextOptions<OnlineTestDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Test> Tests { get; set; }
+    }
+}
---
+++ MainSolution/ModelClass/Test/Test.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ModelClass.test
+{
+    [Table("test", Schema = "public")] // Schema = public cho Postgre
+    public class Test
+    {
+        [Key]
+        public int test_id { get; set; }
+
+        public string? title { get; set; }
+        public string? description { get; set; }
+        public DateTime? created_at { get; set; }
+    }
+}
